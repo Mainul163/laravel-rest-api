9cc5f7f75bab3c79acec89c40137c9fc2d676512
unknown <mainulisraf163@gmail.com>
2023-09-18 18:04:54 +0600

    working

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -1,11 +1,43 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
+use App\Http\Controllers\BaseController as BaseController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
-class RegisterController extends Controller
+use Validator;
+use App\Models\User;
+class RegisterController extends BaseController
 {
     //
+
+    public function register(Request $request){
+
+           $validator=Validator::make($request->all(),[
+              
+            'name'     => 'required|string|max:255',
+            'email'    =>  'required|email|max:255|unique:users',
+            'password' =>  'required|min:6',
+            'confirm_password' =>'required|same:password',  
+           ]);
+
+            if($validator->fails()){
+
+                return $this->sendError('Validation Error',$validator->errors());
+            }
+
+
+           $password = bcrypt($request->password);
+
+           $user=User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>$password
+
+           ]);
+
+           $success['token']=$user->createToken('restApi')->plainTextToken;
+           $success['name']=$user->name;
+
+           return $this->sendResponse($success,'user registration successfully');
+    }
 }
