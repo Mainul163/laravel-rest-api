00f815693a0f98653556335b549b362f06262606
unknown <mainulisraf163@gmail.com>
2023-09-18 13:41:24 +0600

    baseController done

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -7,4 +7,37 @@ use Illuminate\Http\Request;
 class BaseController extends Controller
 {
     //
+    // ******** return success message *********
+
+
+    public function sendResponse($result,$message){
+        $response=[
+            'success'=>true,
+            'data'=>$result,
+            'message'=>$message
+        ];
+
+        return response()->json($response,200);
+    }
+
+     // ******** return error message *********
+
+
+     public function sendError($error,$errorMessage=[],$code=400){
+                $response=[
+               'success'=>false,
+               'message'=>$error
+                ];
+
+                if(!empty($errorMessage)){
+                    $response['data']=$errorMessage;
+                }
+
+                return response()->json($response,$code);
+
+
+     }
+
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('register',[RegisterController::class,'register']);
+Route::post('login',[RegisterController::class,'login']);
+
+
+Route::group(['middleware'=>'auth:sanctum'],function(){
+
+Route::resource('products',ProductController::class);
+
+
 });
