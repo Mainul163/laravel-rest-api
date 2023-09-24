802a1eea72379fb2609b7b75ee9c3f0052a03710
unknown <mainulisraf163@gmail.com>
2023-09-24 17:10:35 +0600

    login register done

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\BaseController as BaseController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Validator;
+use Auth;
 use App\Models\User;
 class RegisterController extends BaseController
 {
@@ -40,4 +41,48 @@ class RegisterController extends BaseController
 
            return $this->sendResponse($success,'user registration successfully');
     }
+
+
+
+
+
+
+
+
+
+        public function login(Request $request){
+ 
+            $validator=Validator::make($request->all(),[
+              
+             
+                'email'    =>  'required|email|max:255',
+                'password' =>  'required|min:6',
+              
+               ]);
+
+               if($validator->fails()){
+
+                return $this->sendError('Validation Error',$validator->errors());
+            }
+
+
+
+            if(Auth::attempt(['email'=>$request->email,'password'=>$request->password]) ){
+
+                $user=Auth::user();
+                $success['token']=$user->createToken('restApi')->plainTextToken;
+                $success['name']=$user->name;
+
+                return $this->sendResponse($success,'user logged in successfully');
+
+            }else{
+
+              return $this->sendError('Unauthorized',['error'=>'Unauthorized']); 
+             
+
+            }
+
+
+      }
+
 }
